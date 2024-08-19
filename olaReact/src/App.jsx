39a2f1230ff18bdaeab1a431f365033d6c15c7ef
jaime-ac <jaimeantonioca97@gmail.com>
2024-08-19 17:07:59 -0300

    Adicionado OlaReact

---
+++ olaReact/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2 className='titulo2'>Olá React!!!</h2>
+      <p className='paragrafos'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente cupiditate incidunt magni, sit voluptatibus placeat libero earum magnam et nesciunt repellat mollitia pariatur est doloremque unde tenetur excepturi ex iusto.</p>
+      
+    </>
+  )
+}
+
+export default App
