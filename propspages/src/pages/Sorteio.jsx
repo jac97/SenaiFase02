941e46c2884f291604782fdd52c9734bf87018db
jaime-ac <jaimeantonioca97@gmail.com>
2024-08-28 23:49:06 -0300

    Versão atualizada do jogo de adivinhar a altura do dinossauro

--- propspages/src/pages/Sorteio.jsx
+++ propspages/src/pages/Sorteio.jsx
@@ -1,38 +1,33 @@
-import Titulos from "../components/Titulos"
-import "./Sorteio.css"
+import Titulos from "../components/Titulos";
+import "./Sorteio.css";
 
-let num_sorteiado, tamanho_dino = 35
+let num_sorteado,
+  tamanho_dino = 35;
 
 function Sorteio() {
+  function Sorteiar() {
+    num_sorteado = Math.floor(Math.random() * 101);
 
-    function Sorteiar(){
-
-        num_sorteiado = Math.floor(Math.random() * 101)
-
-        if (num_sorteiado === tamanho_dino){
-    
-            alert(`Parabéns! Acertou o tamanho do dinossauro👏🏿👏🏿👏🏿`)
-    
-        }else{
-    
-            alert(`Errado! Você não acertou o tamanho do dinossauro😥😥😥`)
-    
-        }
+    if (num_sorteado === tamanho_dino) {
+      alert(
+        `Parabéns! Acertou a altura do dinossauro👏🏿👏🏿👏🏿\n\nAltura do Dinossauro: ${tamanho_dino} metros.\n\nAltura sorteada: ${num_sorteado} metros.`
+      );
+    } else {
+      alert(
+        `Errado! Você não acertou a altura do dinossauro😥😥😥\n\nAltura sorteada: ${num_sorteado} metros.`
+      );
     }
-
+  }
 
   return (
-
     <div className="sorteio-container">
+      <Titulos texto={"Sorteio de Altura do Dinossauro"} emoji={"🎲"} />
 
-        <Titulos texto={"Sorteio"} emoji={"🎲"}/>
-
-        <button className="botao-sorteio" onClick={Sorteiar}>Adivinhar tamanho</button>
-      
+      <button className="botao-sorteio" onClick={Sorteiar}>
+        Adivinhar tamanho
+      </button>
     </div>
-
-  )
-
+  );
 }
 
-export default Sorteio
+export default Sorteio;
