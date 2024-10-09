c2d598b714f2cd1aa6ad692518af95cc98a71e18
jaime-ac <jaimeantonioca97@gmail.com>
2024-10-09 17:01:19 -0300

    add da tela de cadastro do SA na sua pasta

---
+++ sa-segunda-fase/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import TelaCadastro from './pages/TelaCadastro'
+
+function App() {
+  
+  return (
+    <>
+
+      <div className='app-container'>
+
+        <TelaCadastro/>
+        
+      </div>
+      
+    </>
+  )
+}
+
+export default App
